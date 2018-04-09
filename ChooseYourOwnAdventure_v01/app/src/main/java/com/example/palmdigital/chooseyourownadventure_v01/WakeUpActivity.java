8af9f16b8c7ad9c849151e8a783c26627310e460
91091038 <91091038@mvusd.net>
2018-04-09 10:20:35 -0700

    finished CYOA basic

--- ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/WakeUpActivity.java
+++ ChooseYourOwnAdventure_v01/app/src/main/java/com/example/palmdigital/chooseyourownadventure_v01/WakeUpActivity.java
@@ -6,13 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class WakeUpActivity extends AppCompatActivity
+public class WakeUpActivity extends AppCompatActivity implements View.OnClickListener
 {
+Button buttonRestart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wake_up);
+
+        buttonRestart = findViewById(R.id.buttonRestart);
+        buttonRestart.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+        Intent i = new Intent (this, MainActivity.class);
+        startActivity(i);
     }
 
 
